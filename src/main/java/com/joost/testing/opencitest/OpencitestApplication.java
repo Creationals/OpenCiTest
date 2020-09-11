555d82b770755f5c1153970c89542a67dfaaa982
Joost Haverkort <dev@joostit.com>
2020-09-11 13:35:26 +0200

    Initial project commit

---
+++ src/main/java/com/joost/testing/opencitest/OpencitestApplication.java
@@ -0,0 +1,13 @@
+package com.joost.testing.opencitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencitestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joost/testing/opencitest/OpencitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joost.testing.opencitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
