c5348394dcfcac6146235b723115b0a57e819f55
Joost Haverkort <j.haverkort@isential.nl>
2020-09-15 11:48:02 +0200

    Added a failing unit test

--- src/test/java/com/joost/testing/opencitest/OpencitestApplicationTests.java
+++ src/test/java/com/joost/testing/opencitest/OpencitestApplicationTests.java
@@ -2,12 +2,16 @@ package com.joost.testing.opencitest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class OpencitestApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Assert.isTrue(false);
+
     }
 
 }
